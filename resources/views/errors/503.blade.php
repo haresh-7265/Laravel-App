ad9a1c156b3182562be318028423c8f368aff078
Haresh <hareshahir6600@gmail.com>
2026-03-09 10:57:09 +0530

    2.2 – Maintenance Mode

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Maintenance</title>
+</head>
+<body>
+    <h1>Application is under maintenance</h1>
+    <p>Please try again later.</p>
+</body>
+</html>
