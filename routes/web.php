41753b70c7c80521de148415fe6ab7ecfc95b617
Haresh <hareshahir6600@gmail.com>
2026-03-17 18:48:40 +0530

    7.1 – Using Built-in Facades in /facade-test route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TestController;
 use App\Http\Middleware\LogRequestMiddleware;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,3 +27,21 @@ Route::get('/pay', [PaymentController::class, 'pay']);
 Route::get('/test-service', [TestController::class, 'test']);
 
 Route::get('custom-sp',[TestController::class,'message']);
+
+Route::get('/facade-test',function(){
+    Cache::put('name','laravel-12');
+    $name = Cache::get('name','laravel-app');
+    Log::info("Cache Value: ". $name);
+
+    $users = DB::table('users')->get();
+    $userCount = count($users);
+
+    File::put(storage_path('app/test.txt'),'File facade');
+    $content = File::get(storage_path('app/test.txt'));
+
+    return [
+        'name'=>$name,
+        'usersCount'=>$userCount,
+        'fileContent'=>$content
+    ];
+});
