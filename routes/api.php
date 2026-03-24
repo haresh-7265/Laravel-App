e96c8eb4e96fa56f710a8c6c6871ebc2cda6c9de
Haresh <hareshahir6600@gmail.com>
2026-03-24 11:35:27 +0530

    feat: add rate limiting on api route

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Models\Product;
+use App\Facades\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', function (Request $request) {
-    $products = Product::all();
-    return response()->json([
-        'status'=>true,
-        'message'=>'Product data fatched successfully',
-        'data'=>$products->toArray()
-    ]);
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/products', function (Request $request) {
+        $products = Products::getAll();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product data fatched successfully',
+            'data' => $products->toArray()
+        ]);
+    });
 });
